9dce5b6791fe9886a0cfad98bb6c53129bacfecf
ryzen <ryzensingh@gmail.com>
2024-11-01 15:33:54 +0530

    Itemfetching

--- server1/routes/item.routes.js
+++ server1/routes/item.routes.js
@@ -8,7 +8,7 @@ router.get('/searchCategories', itemController.searchItemCategories);
 router.get('/allCategories', itemController.getAllItemCategories);
 
 router.post('/', authenticate, authorize(['admin', 'manager']), itemController.createItem);
-router.get('/', authenticate, itemController.getAllItems);
+router.get('/', itemController.getAllItems);
 router.get('/:id', authenticate, itemController.getItemById);
 router.put('/:id', authenticate, authorize(['admin', 'manager']), itemController.updateItem);
 router.delete('/:id', authenticate, authorize(['admin']), itemController.deleteItem);
